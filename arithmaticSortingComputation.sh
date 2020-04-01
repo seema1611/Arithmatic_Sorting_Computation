fcd7051ca8889a5bf16455b13a9036caa84b79c5
seema1611 <seemarajpure16@gmail.com>
2020-04-01 16:21:16 +0530

    ADDED: All result store in dictionary

--- arithmaticSortingComputation.sh
+++ arithmaticSortingComputation.sh
@@ -1,5 +1,49 @@
 #! /bin/bash -x
 
-read -p "Enter the Numbers a b and c: " a b c
-modAdd=$(( ($a % $b) + $c))
-echo $modAdd
+#Declaration of array
+declare -a arithmaticExpression
+
+function getAddMulti() {
+
+	output1=$(( $(($a + $b)) * $c))
+}
+
+function getMultiAdd() {
+
+   output2=$(( $(($a * $b)) + $c))
+}
+
+function getAddDiv() {
+
+   output3=$(( $(($c + $a)) / $b))
+}
+
+function getModAdd() {
+
+   output4=$(( $(($a % $b)) + $c))
+}
+
+#function to store data in dictionary
+function storeInDictionary() {
+
+	for(( i=1; i<5; i++ ))
+	do
+		arithmaticExpression[result$i]=$((output$i))
+		echo [result$i]=$arithmaticExpression
+	done
+}
+
+#calling function
+function arithmaticSortingComputation() {
+
+	getAddMulti
+	getMultiAdd
+	getAddDiv
+	getModAdd
+	storeInDictionary
+
+}
+
+read -p "Enter the number a b and c: " a b c
+#first function
+arithmaticSortingComputation
